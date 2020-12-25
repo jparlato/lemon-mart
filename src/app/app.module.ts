a5707af339384d2012ecd4f02fc34ec69e4a1411
John P. Parlato <jparlato@mindspring.com>
2020-12-25 10:17:22 -0500

    Adding feature modules and beging app routes based on app design and roles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,35 @@
 import { NgModule } from '@angular/core'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatToolbarModule } from '@angular/material/toolbar'
 import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { HomeComponent } from './home/home.component'
+import { InventoryModule } from './inventory/inventory.module'
+import { ManagerModule } from './manager/manager.module'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+import { PosModule } from './pos/pos.module'
+import { UserModule } from './user/user.module'
+
+const matmodules = [MatToolbarModule, MatButtonModule, MatIconModule]
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    matmodules,
+    ManagerModule,
+    InventoryModule,
+    PosModule,
+    UserModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })

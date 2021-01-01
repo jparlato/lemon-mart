7bc3b2a8713cf56f2154b84d71689ef232647026
John P. Parlato <jparlato@mindspring.com>
2020-12-31 19:30:41 -0500

    fix app module import of materials, create user logon and profile features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core'
 import { FlexLayoutModule } from '@angular/flex-layout'
-import { MatButtonModule } from '@angular/material/button'
 import { MatIconModule } from '@angular/material/icon'
-import { MatToolbarModule } from '@angular/material/toolbar'
 import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
@@ -11,11 +9,12 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HomeComponent } from './home/home.component'
 import { InventoryModule } from './inventory/inventory.module'
+import { MaterialModule } from './material.module'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 import { PosModule } from './pos/pos.module'
 import { UserModule } from './user/user.module'
 
-const matmodules = [MatToolbarModule, MatButtonModule, MatIconModule]
+// const matmodules = [MatToolbarModule,  MatButtonModule, MatIconModule]
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
@@ -25,7 +24,7 @@ const matmodules = [MatToolbarModule, MatButtonModule, MatIconModule]
     BrowserAnimationsModule,
     FlexLayoutModule,
     HttpClientModule,
-    matmodules,
+    MaterialModule,
     InventoryModule,
     PosModule,
     UserModule,
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 
+import { MaterialModule } from '../material.module'
+import { LogoutComponent } from './logout/logout.component'
+import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component'
+import { ProfileComponent } from './profile/profile.component'
 import { UserRoutingModule } from './user-routing.module'
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, UserRoutingModule],
+  declarations: [ProfileComponent, LogoutComponent, NavigationMenuComponent],
+  imports: [CommonModule, MaterialModule, UserRoutingModule],
 })
 export class UserModule {}

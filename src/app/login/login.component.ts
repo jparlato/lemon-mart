e3bc14092682283b82c341fa192076ac09efbff3
jparlato@mindspring.com <jparlato@mindspring.com>
2021-02-10 06:55:15 -0500

    building logon form component.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,24 +1,62 @@
-import { Component, OnInit } from '@angular/core'
-import { FormBuilder, FormGroup } from '@angular/forms'
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { ActivatedRoute, Router } from '@angular/router'
+import { combineLatest } from 'rxjs'
+import { catchError, filter, tap } from 'rxjs/operators'
 import { AuthService } from 'src/app/auth/auth.service'
+import { SubSink } from 'subsink'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
+  private subs = new SubSink()
+
   loginForm!: FormGroup
+  loginError = ''
+  redirectUrl = ''
+
   constructor(
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
-    private authenticationService: AuthService
+    private authService: AuthService
   ) {}
 
-  login(): void {
-    return
+  ngOnInit(): void {
+    this.authService.logout()
+    this.buildLoginForm()
   }
 
-  ngOnInit(): void {}
+  buildLoginForm(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [Validators.required, Validators.minLength(8), Validators.maxLength(50)],
+      ],
+    })
+  }
+
+  async login(submittedForm: FormGroup): Promise<void> {
+    this.authService
+      .login(submittedForm.value.email, submittedForm.value.password)
+      .pipe(catchError((err) => (this.loginError = err)))
+    this.subs.sink = combineLatest([
+      this.authService.authStatus$,
+      this.authService.currentUser$,
+    ])
+      .pipe(
+        filter(([authStatus, user]) => authStatus.isAuthenticated && user?._id !== ''),
+        tap(([authStatus, user]) => {
+          this.router.navigate([this.redirectUrl || '/manager'])
+        })
+      )
+      // tslint:disable-next-line: align
+      .subscribe()
+  }
+  ngOnDestroy(): void {
+    this.subs.sink?.unsubscribe()
+  }
 }
